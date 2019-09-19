d7ba8ea5822f4b1585dc12ec84672957ef648ebf
Dalton Sydnor <dms6298@psu.edu>
2019-09-19 19:50:06 -0400

    Added Packages

--- IST412_Group1/src/ist412_group1/IST412_Group1.java
+++ IST412_Group1/src/ist412_group1/IST412_Group1.java
@@ -9,10 +9,7 @@ import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
-/**
- *
- * @author Dalton
- */
+
 public class IST412_Group1 extends Application {
     
     @Override
@@ -37,9 +34,7 @@ public class IST412_Group1 extends Application {
         primaryStage.show();
     }
 
-    /**
-     * @param args the command line arguments
-     */
+
     public static void main(String[] args) {
         launch(args);
     }
