2c30b970b1f78998c70259ad4c795fa407b768ed
Dalton Sydnor <dms6298@psu.edu>
2019-09-22 14:03:02 -0400

    90% Comments Added

--- IST412_Group1/src/ist412_group1/View/careInstructionsView.java
+++ IST412_Group1/src/ist412_group1/View/careInstructionsView.java
@@ -1,4 +1,11 @@
-//View layout of careinstructions
+/**View layout of care instructions
+Controlled by
+-Controller.careInstructionsViewCntl
+Initiated by
+-Controller.IST412_Group1
+*/
+
+
 package ist412_group1.View;
 
 /**
