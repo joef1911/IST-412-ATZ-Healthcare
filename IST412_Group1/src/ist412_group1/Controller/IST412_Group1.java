ac3e25d6cb1d4c6542d514d44601716ab3943463
xiaohan <xiaohan@10.0.0.48>
2019-09-22 21:21:48 -0400

    Added some comments. Created getters and setters for patient class.

--- IST412_Group1/src/ist412_group1/Controller/IST412_Group1.java
+++ IST412_Group1/src/ist412_group1/Controller/IST412_Group1.java
@@ -1,7 +1,8 @@
-/**Main Controller that interacts and initializes all other controllers
-Starting point of the Application
-Initializes signInView from the start of the app
-*/
+/**
+ * Main Controller that interacts and initializes all other controllers
+ * Starting point of the Application.
+ * Initializes signInView from the start of the app.
+ */
 package ist412_group1.Controller;
 
 import javafx.application.Application;
@@ -11,16 +12,25 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+import ist412_group1.Model.*;
+import ist412_group1.View.*;
 
 
 public class IST412_Group1 extends Application {
     
+    /**
+     * This is the methods that starts the UI.
+     * @param primaryStage 
+     */
     @Override
     public void start(Stage primaryStage) {
             
     }
 
-
+    /**
+     * This is the main method.
+     * @param args 
+     */
     public static void main(String[] args) {
         launch(args);
     }
