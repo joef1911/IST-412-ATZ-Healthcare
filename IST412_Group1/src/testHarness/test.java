62360261c901d293572d9a9382306a826573b02b
Dalton Sydnor <dms6298@psu.edu>
2019-10-06 15:58:00 -0400

    Added TestHarness

---
+++ IST412_Group1/src/testHarness/test.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testHarness;
+import static javafx.application.Application.launch;
+
+
+/**
+ *
+ * @author Dalton
+ */
+
+
+public class test {
+    
+    
+    
+    
+    
+     public static void main(String[] args) {       
+        launch(args);
+    }
+     
+     
+
+}
