c6a22df179e52812544142f1c12d19998fdfb0a0
Dalton Sydnor <dms6298@psu.edu>
2019-10-06 16:28:34 -0400

    Test 1 Maybe

---
+++ IST412_Group1/src/ist412_group1/Model/User.java
@@ -0,0 +1,40 @@
+/**
+ * Data model of user info
+ * Data is pulled from here for
+ *      -View.signInView
+ * Interacts with
+ *      -Controller.IST412_Group1
+*/
+package ist412_group1.Model;
+
+import java.util.ArrayList;
+
+public class User {
+    private String type;
+    String firstName;
+    String lastName;
+    String userName;
+    String password;
+    int loggedIn = 0;
+    
+    /**
+     * This is the method that enables user to log in.
+     * @param a the username
+     * @param b the password
+     * @return a - username
+     */
+    public User(String a, String b, String c, String d){
+        firstName = a;
+        lastName = b;
+        userName = c;
+        password = d;
+    }
+    
+    public String login(String a, String b){
+        
+        if(a == userName && b ==password){
+            loggedIn = 1;
+        }
+        return a;
+    }
+}
