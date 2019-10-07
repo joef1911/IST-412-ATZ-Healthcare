19ddf1dabdcb3388acf52922f6a50df00597924e
Joe Fleck <Joe Fleck@A-Desktop>
2019-10-06 22:11:19 -0400

    Added tests to check if patient data is null --!!Does not run

--- IST412_Group1/test/ist412_group1/testHarness/UnitTest.java
+++ IST412_Group1/test/ist412_group1/testHarness/UnitTest.java
@@ -7,6 +7,7 @@ package ist412_group1.testHarness;
 import ist412_group1.Controller.*;
 import ist412_group1.Model.*;
 import ist412_group1.View.*;
+import java.util.ArrayList;
 import static javafx.application.Application.launch;
 import static junit.framework.Assert.*;
 import org.junit.Test;
@@ -17,16 +18,27 @@ import org.junit.Test;
 
 
 public class UnitTest {
-        private static UserDataBase base = new UserDataBase();
-        private static User user = base.getDataBase().get(1);
-        private static String firstName = base.getDataBase().get(1).getFirstName();
-    
+        private final static UserDataBase base = new UserDataBase();
+        private final static User user = base.getDataBase().get(1);
+        private final static String firstName = base.getDataBase().get(1).getFirstName();
+        
+        private final static PatientDatabase patientBase = new PatientDatabase();
+        private final static ArrayList<String> patientData = patientBase.getPatient1Data();
+        private final static String patientId = patientBase.getPatient1Id();
      
-     @Test
-     public void testDataBase(){
-         assertNotNull("Database is Populated", base);
-         assertNotNull("User Populated", user);
-         assertNotNull("User info Populated", firstName);
-     }  
+    @Test
+    public void testDataBase(){
+        assertNotNull("Database is Populated", base);
+        assertNotNull("User Populated", user);
+        assertNotNull("User info Populated", firstName);
+    }
+    
+    @Test
+    public void testPatientData(){
+        assertNotNull("Patient database exists", patientBase);
+        assertNotNull("Patient database is populated", patientData);
+        assertNotNull("Patient has a patient ID", patientId);
+    }
+    
 
 }
