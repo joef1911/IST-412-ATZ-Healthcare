9c8dd8214ddfae1a00e857ab294f79f189286b6a
Dalton Sydnor <dms6298@psu.edu>
2019-11-23 21:24:38 -0500

    Refactor

--- IST412_Group1/src/ist412_group1/View/AutocompleteTextField.java
+++ IST412_Group1/src/ist412_group1/View/AutocompleteTextField.java
@@ -33,7 +33,15 @@ public class AutocompleteTextField extends TextField {
     } 
     
     private void initWordBase() {
-        PatientDatabase p = new PatientDatabase(new IST412_Group1());        
+        PatientDatabase p = new PatientDatabase(new IST412_Group1());
+        //patientCount Method Unavailable
+//        for(int i = 0; i< patientCount; i++) {
+//            patient patient = p.getPatient(i);
+//            element.add(patient.getPatientName());
+//            element.add(patient.getPatientId());
+//            
+//        }
+        
     }
 
     private void startListen() {     
